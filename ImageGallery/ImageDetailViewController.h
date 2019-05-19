971d168141ffcfef3714bf8a7099c5478df7c1e2
Ray Kang <adamck6302438@gmail.com>
2019-05-19 11:05:00 -0400

    first commit

---
+++ ImageGallery/ImageDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageDetailViewController.h
+//  ImageGallery
+//
+//  Created by Frances ZiyiFan on 5/13/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
